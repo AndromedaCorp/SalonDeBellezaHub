49528a61acc9c6158a0c59fed66de85d5f62fbf1
HectorBonilla <andromedaCorp3.0@outlook.com>
2021-10-04 23:48:19 -0600

    Hector Bonilla: Modificacion a algunos metodos de la Clase UsuarioController WebAPI

--- SalonBelleza.WebAPI/Controllers/UsuarioController.cs
+++ SalonBelleza.WebAPI/Controllers/UsuarioController.cs
@@ -108,16 +108,29 @@ namespace SalonBelleza.WebAPI.Controllers
             return usuarios;
 
         }
+
         [HttpPost("Login")]
-        public async Task<Usuario> Login([FromBody] object pUsuario)
+        [AllowAnonymous]
+        public async Task<ActionResult> Login([FromBody] object pUsuario)
         {
 
             var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
             string strUsuario = JsonSerializer.Serialize(pUsuario);
             Usuario usuario = JsonSerializer.Deserialize<Usuario>(strUsuario, option);
-            return await usuarioBL.LoginAsync(usuario);
-
+            // codigo para autorizar el usuario por JWT
+            Usuario usuario_auth = await usuarioBL.LoginAsync(usuario);
+            if (usuario_auth != null && usuario_auth.Id > 0 && usuario.Login == usuario_auth.Login)
+            {
+                var token = authService.Authenticate(usuario_auth);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+            // *********************************************
         }
+
         [HttpPost("CambiarPassword")]
         public async Task<ActionResult> CambiarPassword([FromBody] Object pUsuario)
         {
