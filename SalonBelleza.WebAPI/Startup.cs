15995a8fc8ca01f1a93f59375528b3cb00df3fed
HectorBonilla <andromedaCorp3.0@outlook.com>
2021-10-07 18:28:25 -0600

    Hector Bonilla: Cambios al Startup de APi web.

--- SalonBelleza.WebAPI/Startup.cs
+++ SalonBelleza.WebAPI/Startup.cs
@@ -33,8 +33,9 @@ namespace SalonBelleza.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
 
-            
+            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SalonBelleza.WebAPI", Version = "v1" });
@@ -96,6 +97,13 @@ namespace SalonBelleza.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("*");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
