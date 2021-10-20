7334675476ea3db7a346c36fa708f87781b148bf
HectorBonilla <andromedaCorp3.0@outlook.com>
2021-10-20 12:00:57 -0600

    Hector Bonilla: Creacion de los archivos iniciales para pruebas unitarias.

---
+++ SalonDeBellezaWebAPI.PruebasUnitarias/CitaDALTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalonBelleza.AccesoADatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonBelleza.AccesoADatos.Tests
+{
+    [TestClass()]
+    public class CitaDALTests
+    {
+        [TestMethod()]
+        public void CrearAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerPorIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerTodosAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarIncluirUsuarioAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarIncluirClienteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarIncluirUsuarioClienteAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
