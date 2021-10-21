b1ec7b7a64c5b0e7f552fe658183bf34687c5fe9
Saul Valdez <saulvaldez472@gmail.com>
2021-10-22 00:29:05 +0200

    Cree pruebas unitarias nuevas, en CitaDAL test

--- SalonDeBellezaWebAPI.PruebasUnitarias/CitaDALTests.cs
+++ SalonDeBellezaWebAPI.PruebasUnitarias/CitaDALTests.cs
@@ -22,7 +22,7 @@ namespace SalonBelleza.AccesoADatos.Tests
             cita.Total = 2;
             cita.Estado = 0;
             cita.FechaCita = DateTime.Now;
-            
+
             //int result = await CitaDAL.CrearAsync(cita);
             //Assert.IsFalse(result == 0);
         }
@@ -32,7 +32,7 @@ namespace SalonBelleza.AccesoADatos.Tests
         {
             Cita cita = new Cita(); //Creamos una instancia de Cita en la cual le agregaremos parametros.
             //Agregamos el Id mas los parametros a modificar, no puede ir ninguno vacio.
-            cita.Id = 46;
+            cita.Id = 5;
             cita.IdUsuario = 1;
             cita.IdCliente = 2;
             cita.Total = 2;
@@ -47,7 +47,7 @@ namespace SalonBelleza.AccesoADatos.Tests
         public async Task EliminarAsyncTest()
         {
             Cita cita = new Cita();
-            cita.Id = 32; //Obtenemos el Id para eliminar el registro.
+            cita.Id = 6; //Obtenemos el Id para eliminar el registro.
 
             int result = await CitaDAL.EliminarAsync(cita); //Le pasamos el objeto con el ID al metodo Eliminar.
             Assert.IsFalse(result == 0);
@@ -58,7 +58,7 @@ namespace SalonBelleza.AccesoADatos.Tests
         {
             //Creamos un objeto de la clase, para luego Indicar el ID que se desea buscar.
             Cita cita = new Cita();
-            cita.Id = 46;
+            cita.Id = 5;
 
             Cita result;
             result = await CitaDAL.ObtenerPorIdAsync(cita); //Agregamos el Id al metodo Obtener, el resultado se lo agregamos a result.
@@ -86,21 +86,50 @@ namespace SalonBelleza.AccesoADatos.Tests
         }
 
         [TestMethod()]
-        public void BuscarIncluirUsuarioAsyncTest()
+        public async Task BuscarIncluirUsuarioAsyncTest()
         {
-            Assert.Fail();
+            Cita cita = new Cita();
+
+            //Agregamos los parametro para buscar.
+            cita.Id = 5;
+            cita.IdUsuario = 1;
+
+            //Agregamos los parametros al metodo buscar, luego este llenara la Lista de Cita con los resultados.
+            List<Cita> citas = await CitaDAL.BuscarIncluirUsuarioAsync(cita);
+            Assert.IsFalse(citas.Count == 0);
+
         }
 
         [TestMethod()]
-        public void BuscarIncluirClienteAsyncTest()
+        public async Task BuscarIncluirClienteAsyncTest()
         {
-            Assert.Fail();
+            Cita cita = new Cita();
+
+            //Agregamos los parametro para buscar.
+            cita.Id = 5;
+            cita.IdCliente = 2;
+
+            //Agregamos los parametros al metodo buscar, luego este llenara la Lista de Cita con los resultados.
+            List<Cita> citas = await CitaDAL.BuscarIncluirClienteAsync(cita);
+            Assert.IsFalse(citas.Count == 0);
         }
 
         [TestMethod()]
-        public void BuscarIncluirUsuarioClienteAsyncTest()
+        public async Task BuscarIncluirUsuarioClienteAsyncTest()
         {
-            Assert.Fail();
+
+            Cita cita = new Cita();
+
+            //Agregamos los parametro para buscar.
+            cita.Id = 5;
+            cita.IdUsuario = 1;
+            cita.IdCliente = 1;
+
+            //Agregamos los parametros al metodo buscar, luego este llenara la Lista de Cita con los resultados.
+            List<Cita> citas = await CitaDAL.BuscarIncluirUsuarioClienteAsync(cita);
+            Assert.IsFalse(citas.Count == 0);
+
+
         }
     }
 }
