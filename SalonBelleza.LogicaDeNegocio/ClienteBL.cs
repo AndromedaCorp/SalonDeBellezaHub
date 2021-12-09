d2bc1a8af8ea4760194979c0e73a53806c0dd81b
Erick-Dono <erickjosef94@outlook.com>
2021-12-08 21:53:13 -0600

    Se documentaron todas las clases BL y sus metodos

--- SalonBelleza.LogicaDeNegocio/ClienteBL.cs
+++ SalonBelleza.LogicaDeNegocio/ClienteBL.cs
@@ -18,27 +18,66 @@ namespace SalonBelleza.LogicaDeNegocio
     /// </summary> 
     public class ClienteBL
     {
+        /// <summary>  
+        /// Metodo para crear un Nuevo cliente.
+        /// </summary>  
+        /// <param name="pCliente">Se espera un objeto del Tipo Cliente, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<int> CrearAsync(Cliente pCliente) 
         {
             return await ClienteDAL.CrearAsync(pCliente);
         }
 
+        /// <summary>  
+        /// Metodo para Modificar un Cliente segun los parametros agregados.
+        /// </summary>  
+        /// <param name="pCliente">Se espera un objeto del tipo Cliente con todas sus propiedades llenas</param>
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<int> ModificarAsync(Cliente pCliente) 
         {
             return await ClienteDAL.ModificarAsync(pCliente);
         }
+
+        /// <summary>  
+        /// Metodo para Eliminar un Cliente segun el Id.
+        /// </summary>  
+        /// <param name="pCliente">Se espera un objeto del Tipo Cliente, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<int> EliminarAsync(Cliente pCliente) 
         {
             return await ClienteDAL.EliminarAsync(pCliente);
         }
+
+        /// <summary>  
+        /// Metodo para obtener por ID un cliente ingresado
+        /// </summary>  
+        /// <param name="pCliente">Se espera un objeto del Tipo Cliente, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<Cliente> ObtenerPorIdAsync(Cliente pCliente) 
         {
             return await ClienteDAL.ObtenerPorIdAsync(pCliente);
         }
+
+        /// <summary>  
+        /// Metodo para Obtener todos los Cliente.
+        /// </summary>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<List<Cliente>> ObtenerTodosAsync() 
         {
             return await ClienteDAL.ObtenerTodosAsync();
         }
+
+        /// <summary>  
+        /// Metodo para Buscar un Cliente Async
+        /// </summary>  
+        /// <param name="pCliente">Se espera un objeto del Tipo Usuario, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        /// 
         public async Task<List<Cliente>> BuscarAsync(Cliente pCliente1) 
         {
             return await ClienteDAL.BuscarAsync(pCliente1);
--- SalonBelleza.LogicaDeNegocio/DetalleCitaBL.cs
+++ SalonBelleza.LogicaDeNegocio/DetalleCitaBL.cs
@@ -29,25 +29,59 @@ namespace SalonBelleza.LogicaDeNegocio
             return await DetalleCitaDAL.CrearAsync(pDetalleCita);
         }
 
+        /// <summary>  
+        /// Con este metodo Actualisaremos en la base de datos el DetalleCita.
+        /// </summary>  
+        /// <param name="pDetalleCita">Se espera un objeto del Tipo DetalleCita, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        ///
+
         public async Task<int> ModificarAsync(DetalleCita pDetalleCita)
         {
             return await DetalleCitaDAL.ModificarAsync(pDetalleCita);
         }
+
+        /// <summary>  
+        /// Metodo para eliminar para Eliminar un DetalleCita en la Base de Datos.
+        /// </summary>  
+        /// <param name="pDetalleCita">Se espera un objeto del Tipo DetalleCita, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        ///
 
         public async Task<int> EliminarAsync(DetalleCita pDetalleCita)
         {
             return await DetalleCitaDAL.EliminarAsync(pDetalleCita);
         }
 
+        /// <summary>  
+        /// Metodo para Obtener por Id un DetalleCita en la base de Datos
+        /// </summary>  
+        /// <param name="pDetalleCita">Se espera un objeto del Tipo DetalleCita, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        ///
+
         public async Task<DetalleCita> ObtenerPorIdAsync(DetalleCita pDetalleCita)
         {
             return await DetalleCitaDAL.ObtenerPorIdAsync(pDetalleCita);
         }
 
+        /// <summary>  
+        /// Metodo para obtener todos los DetalleCita en la base de Datos.'
+        /// </summary>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        ///
+
         public async Task<List<DetalleCita>> ObtenerTodosAsync()
         {
             return await DetalleCitaDAL.ObtenerTodosAsync();
         }
+
+        /// <summary>  
+        /// Metodo para Obtener por Id un DetalleCita en la base de Datos
+        /// </summary>  
+        /// <param name="pCita">Se espera un objeto del Tipo Cita, con sus valores llenos</param>  
+        /// <returns>Retorna una tarea Asyncrona</returns>  
+        ///
 
         public async Task<List<DetalleCita>> BuscarAsync(DetalleCita pDetalleCita)
         {
